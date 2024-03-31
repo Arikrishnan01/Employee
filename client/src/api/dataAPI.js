9dac3f359d85f9f4ca9bd658998c79090e40c0ab
Arikrishnan01 <krishari11052001@gmail.com>
2024-03-31 11:55:41 +0530

    updated

--- client/src/api/dataAPI.js
+++ client/src/api/dataAPI.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BAKCEND_URL = "http://localhost:5000";
+// export const BAKCEND_URL = "http://localhost:5000";
+export const BAKCEND_URL = "https://employee-tv37.onrender.com";
 
 export async function getAllEmp(){
     return axios.get(`${BAKCEND_URL}/emp/getAllEmp`)
